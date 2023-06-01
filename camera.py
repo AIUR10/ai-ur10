bd59f3fcdb80405732d0de9f297a1a94fe64a307
kevin <migadel.kevin@gmail.com>
2023-06-01 22:34:44 +0200

    Video segmentation: Working but too long for real-time video segmentation

--- camera.py
+++ camera.py
@@ -3,17 +3,22 @@
 import cv2
 import logging
 import numpy as np
-from segment_anything.modeling import Sam
-from segment_anything import SamAutomaticMaskGenerator, sam_model_registry
+from transformers import pipeline
+from PIL import Image, ImageDraw
+import numpy as np
+import matplotlib.pyplot as plt
+import gc
+from tqdm import tqdm
 
 
 class Camera():
       
-    def __init__(self, ip_address_camera=None) -> None:
+    def __init__(self, ip_address_camera=None, model="facebook/sam-vit-huge", device=0) -> None:
         if ip_address_camera is not None:
             self.host = ip_address_camera
             self.port = 8080
         self.device = "cuda"
+        self.generator = pipeline("mask-generation", model=model, device=device)
 
         logging.basicConfig(level=logging.INFO)
 
@@ -33,76 +38,67 @@ class Camera():
 
     async def read(self, terminate_flag):
         colors = np.random.randint(0, 255, size=(256, 3), dtype=np.uint8)
+        
+        logging.info("Video segmentation...")
+        length = int(self.con.get(cv2.CAP_PROP_FRAME_COUNT))
+        for _ in tqdm(range(length)):
 
-        while self.con.isOpened():
+            # while self.con.isOpened():
             status, frame = self.con.read()
 
             # if frame is read correctly status is True
             if not status:
                 logging.info("Can't receive frame (stream end?). Exiting ...")
                 break
-
+            
             output = self.segment(frame, colors)
             # output = frame
 
             # Set the desired width and height
-            resized_frame = cv2.resize(output, (self.frame_width//3, self.frame_height//3))  
+            # resized_frame = cv2.resize(output, (self.frame_width//3, self.frame_height//3))  
             # resized_frame = cv2.resize(frame, (self.frame_width//3, self.frame_height//3))  
 
             # # Display the resized frame
-            cv2.imshow('Resized Frame', resized_frame)
+            # cv2.imshow('Resized Frame', resized_frame)
 
-            cv2.waitKey(1)
+            # cv2.waitKey(1)
 
             # Check if termination flag is set
-            if terminate_flag:
+            if terminate_flag and not status:
+                logging.info("Done...")
                 break        
 
     def segment(self, frame, colors, model_type="vit_h", sam_checkpoint="sam_vit_h_4b8939.pth"):
-        
-        logging.info("Creating sam model...")
-        sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
-        sam.to(device=self.device)
-        logging.info("Done")
-        
-        
-        logging.info("Creating mask generator...")
-        mask_generator = SamAutomaticMaskGenerator(sam)
-        logging.info("Done")
+               
+        frame = Image.fromarray(frame.astype('uint8'), 'RGB')
+        outputs = self.generator(frame)
 
-        logging.info("Generating mask...")
-        masks = mask_generator.generate(frame)
-        logging.info("Done")
+        masks = outputs["masks"]
+        combined_image = self.show_masks_on_image(frame, masks)
 
-        combined_mask = None
+        combined_image = cv2.cvtColor(np.array(combined_image), cv2.COLOR_RGB2BGR)
+        self.out.write(combined_image)
 
-        if len(masks) != 0:
-
-            sorted_anns = sorted(masks, key=(lambda x: x["area"]), reverse=True)
-            mask_image = np.zeros(
-                (masks[0]["segmentation"].shape[0], masks[0]["segmentation"].shape[1], 3), dtype=np.uint8
-            )
-
-            for i, ann in enumerate(sorted_anns):
-                m = ann["segmentation"]
-                color = colors[i % 256]
-                img = np.zeros((m.shape[0], m.shape[1], 3), dtype=np.uint8)
-                img[:, :, 0] = color[0]
-                img[:, :, 1] = color[1]
-                img[:, :, 2] = color[2]
-                img = cv2.bitwise_and(img, img, mask=m.astype(np.uint8))
-                img = cv2.addWeighted(img, 0.35, np.zeros_like(img), 0.65, 0)
-                mask_image = cv2.add(mask_image, img)
-            
-            combined_mask = cv2.add(frame, mask_image)
-            self.out.write(combined_mask)
-        
-        return combined_mask
+        return combined_image
 
     def disconnect(self):
         self.con.release()
         self.out.release()
         cv2.destroyAllWindows()
+        
+
+    def show_masks_on_image(self, raw_image, masks):
+        combined_image = Image.fromarray(np.array(raw_image))
+
+        for mask in masks:
+            color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+            h, w = mask.shape[-2:]
+            mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+            mask_image = Image.fromarray((mask_image * 255).astype(np.uint8))
+            combined_image.paste(mask_image, (0, 0), mask_image)
+
+        return combined_image
+        
         
 
 
