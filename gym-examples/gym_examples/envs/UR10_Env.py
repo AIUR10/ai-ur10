b40bd4cf108e415b4f76baac6a21d1666eb98372
JoffreyMa <joffreyma@yahoo.fr>
2023-06-18 15:23:54 +0200

    Env gym where the robot arm actually move

--- gym-examples/gym_examples/envs/UR10_Env.py
+++ gym-examples/gym_examples/envs/UR10_Env.py
@@ -18,7 +18,8 @@ from pyrep.robots.arms.ur10 import UR10
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
 
-SCENE_FILE = join(os.getcwd(), 'scene_with_ur10.ttt')
+#SCENE_FILE = join(os.getcwd(), 'scene_with_ur10.ttt')
+SCENE_FILE = join(os.getcwd(), 'test_scene_robots_ur10.ttt')
 EPISODES = 2
 EPISODE_LENGTH = 3
 
@@ -27,13 +28,16 @@ class UR10Env(gym.Env):
     def __init__(self):
         super(UR10Env, self).__init__()
         self.pr = PyRep()
-        self.pr.launch(SCENE_FILE, headless=False) # headless=True introduces error in notebook
+        self.pr.launch(SCENE_FILE, headless=False) # headless=True not to display the CoppeliaSim interface
         self.pr.start()
         self.agent = UR10()
-        print(self._get_state())
-        print(f"Simulation time step: {self.pr.get_simulation_timestep()} seconds")
+        self.agent.set_control_loop_enabled(False)
+        self.agent.set_motor_locked_at_zero_velocity(True)
+        #print(self._get_state())
+        #print(f"Simulation time step: {self.pr.get_simulation_timestep()} seconds")
+        self.agent_ee_tip = self.agent.get_tip()
+        self.initial_joint_positions = self.agent.get_joint_positions()
         self.total_distance_moved = 0  # Add a counter for total distance moved
-        #self.reset()
         
     def _get_state(self):
         # Return state containing arm joint angles/velocities & target position
@@ -41,14 +45,15 @@ class UR10Env(gym.Env):
                             self.agent.get_joint_velocities()])
         
     def reset(self):
-        return None
+        self.agent.set_joint_positions(self.initial_joint_positions)
+        return self._get_state()
 
     def step(self, action):
-        initial_position = self.agent.get_tip().get_position()  # Get the initial position of the end effector
-        velocities = [0.1, 0, 0, 0, 0, 0]  # Adjust these velocities until the arm moves in the desired direction
+        initial_position = self.agent_ee_tip.get_position()  # Get the initial position of the end effector
+        velocities = [10, 10, 10, 10, 10, 10]  # Adjust these velocities until the arm moves in the desired direction
         self.agent.set_joint_target_velocities(velocities)  # Apply velocities
         self.pr.step()  # Step the physics simulation forward
-        final_position = self.agent.get_tip().get_position()  # Get the final position of the end effector
+        final_position = self.agent_ee_tip.get_position()  # Get the final position of the end effector
 
         # Calculate distance moved and add to total
         distance_moved = np.linalg.norm(np.array(final_position) - np.array(initial_position))
